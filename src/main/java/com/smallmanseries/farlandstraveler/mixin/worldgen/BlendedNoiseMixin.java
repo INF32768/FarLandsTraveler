c632c7e33d53670b98a658b1330684119366f99f
INF32768 <3478658394@qq.com>
2026-03-10 14:06:59 +0800

    refactor: better overflowable blended noise

---
+++ src/main/java/com/smallmanseries/farlandstraveler/mixin/worldgen/BlendedNoiseMixin.java
@@ -0,0 +1,34 @@
+package com.smallmanseries.farlandstraveler.mixin.worldgen;
+
+import com.smallmanseries.farlandstraveler.common.worldgen.densityfunctions.BlendedNoiseOverflowable;
+import net.minecraft.world.level.levelgen.synth.BlendedNoise;
+import net.minecraft.world.level.levelgen.synth.PerlinNoise;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * 将原版的混合噪声改造成有特定变量控制溢出的混合噪声，为 {@link BlendedNoiseOverflowable} 类提供服务。
+ *
+ * @see IBlendedNoiseMixin
+ *
+ * @author INF32768
+ */
+@Mixin(BlendedNoise.class)
+public abstract class BlendedNoiseMixin {
+    @Unique
+    boolean _$isOverflowable = false;
+
+    @Inject(method = "<init>(Lnet/minecraft/world/level/levelgen/synth/PerlinNoise;Lnet/minecraft/world/level/levelgen/synth/PerlinNoise;Lnet/minecraft/world/level/levelgen/synth/PerlinNoise;DDDDD)V", at = @At("RETURN"))
+    private void addOverflowableFlag(PerlinNoise minLimitNoise, PerlinNoise maxLimitNoise, PerlinNoise mainNoise, double xzScale, double yScale, double xzFactor, double yFactor, double smearScaleMultiplier, CallbackInfo ci) {
+        this._$isOverflowable = BlendedNoiseOverflowable.createOverflowable;
+    }
+
+    @Redirect(method = "compute", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/synth/PerlinNoise;wrap(D)D"))
+    private double redirectWrap(double value) {
+        return this._$isOverflowable ? PerlinNoise.wrap(value) : value;
+    }
+}
