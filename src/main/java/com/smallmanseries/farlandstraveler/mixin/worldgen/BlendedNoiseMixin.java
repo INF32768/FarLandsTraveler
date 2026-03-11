47133b07e8d5e6aabc5363a7c3182697cd9a6929
SmallmanSeries <963499487@qq.com>
2026-03-11 14:05:04 +0800

    optimize import

--- src/main/java/com/smallmanseries/farlandstraveler/mixin/worldgen/BlendedNoiseMixin.java
+++ src/main/java/com/smallmanseries/farlandstraveler/mixin/worldgen/BlendedNoiseMixin.java
@@ -4,11 +4,8 @@ import com.smallmanseries.farlandstraveler.common.worldgen.densityfunctions.Blen
 import net.minecraft.world.level.levelgen.synth.BlendedNoise;
 import net.minecraft.world.level.levelgen.synth.PerlinNoise;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * 将原版的混合噪声改造成基于派生类类型控制溢出的混合噪声，若为 {@link BlendedNoiseOverflowable} 派生类则溢出。
